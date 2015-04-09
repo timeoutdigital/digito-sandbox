5dca4ad605859a83f7178abd06cd2db3d6f524ea
superstardonkeyjockey <waynepeacock@timeout.com>
2015-04-09 16:27:53 +0100

    moved selenium into here from digito just to play with

---
+++ selenium/IDE/data.js
@@ -0,0 +1,7 @@
+﻿var urls=["/london","/paris"];
+var headings=["Your ultimate guide to London","Paris"];
+
+var hubheadings=[
+["Things to Do","Food & Drink","Theatre & The Arts"],
+["Restaurants & Cafés","Bars & Pubs","Film","Music & Nightlife","Things to Do"]
+];
